cce74d0c32b3e4a04baa770116e73e6d04318ea2
Tenzija <milosmiskov.miki@gmail.com>
2023-03-01 19:30:37 +0100

    work work work

--- src/components/ui/common/breadcrumbs/index.js
+++ src/components/ui/common/breadcrumbs/index.js
@@ -1,11 +1,17 @@
-export default function Breadcrumbs() {
+import Link from "next/link"
+
+export default function Breadcrumbs({items}) {
 
   return (
     <nav aria-label="breadcrumb">
       <ol className="flex leading-none text-gray-500 divide-x divide-indigo-600">
-        <li className="pr-4 hover:text-gray-900"><a href="#">Buy</a></li>
-        <li className="px-4 hover:text-gray-900"><a href="#">My Orders</a></li>
-        <li className="px-4 hover:text-gray-900"><a href="#">All Orders</a></li>
+        { items.map((item, index) =>
+          <li key={item.href} className={`${index === 0 ? 'pr-4' : 'px-4'} hover:text-gray-900`}>
+            <Link href={item.href}>
+              <a>{item.value}</a>
+            </Link>
+          </li>
+        )}
       </ol>
     </nav>
   )
--- src/components/ui/marketplace/header/index.js
+++ src/components/ui/marketplace/header/index.js
@@ -3,12 +3,25 @@ import { EthRates, WalletBar } from "../../web3";
 
 export default function Header() {
 
+    const LINKS = [{
+        href: '/marketplace',
+        value: 'Buy'
+    }, {
+        href: '/marketplace/courses/owned',
+        value: 'Owned'
+    }, {
+        href: '/marketplace/courses/managed',
+        value: 'Manage'
+    }]
+
     return (
         <>
             <WalletBar />
             <EthRates />
             <div className='flex flex-row-reverse pb-4 px-4 sm:px-6 lg:px-8'>
-                <Breadcrumbs />
+                <Breadcrumbs
+                    items={LINKS}
+                />
             </div>
         </>
     )
