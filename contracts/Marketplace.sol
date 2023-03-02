930a9f9bd25fa5256dce307df30731b46b964ee5
Tenzija <milosmiskov.miki@gmail.com>
2023-03-02 17:58:13 +0100

    Update Marketplace.sol

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -10,24 +10,39 @@ contract Marketplace {
 
 	struct Item {
 		uint id;
-		uint price;
-		bytes32 proof; // 0x0000000000000000000000000000313000000000000000000000000000003130 placeholder 
+		uint price; 
+		bytes32 proof; 
 		address owner;
 		State state;
 	}
 
-	function purchaseItem(bytes16 _itemId, bytes32 _proof) external payable returns(bytes32) {
-		// item id - 10
-		// 0x00000000000000000000000000003130 hex value so it fits the bytes16 format of the itemId
+	// mapping itemHash to Item data
+	mapping(bytes32 => Item) public ownedItems;
 
-		// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4 msg.sender
-		// 0x00000000000000000000000000031305B38Da6a701c568545dCfcB03FcB875f56beddC4 
-		// keccak256 hash of the item id and the msg.sender
-			// site - c4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
-			// function itemHash - c4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
-		// WORKS!!!
+	// mapping of itemId to ItemHash			
+	mapping(uint => bytes32) public ownedItemsHash;
 
+	// number of all items + id of the items
+	uint private totalOwnedItems;
+
+	function purchaseItem(
+		bytes16 _itemId, // 0x00000000000000000000000000003130 hex value so it fits the bytes16 format of the itemId
+		bytes32 _proof // 0x0000000000000000000000000000313000000000000000000000000000003130 placeholder 
+	) external payable {
 		bytes32 itemHash = keccak256(abi.encodePacked(_itemId, msg.sender));
-		return itemHash;
+		// keccak256 hash of the item id and the msg.sender
+			// site - 0xc4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
+			// function itemHash - 0xc4eaa3558504e2baa2669001b43f359b8418b44a4477ff417b4b007d7cc86e37
+		// WORKS!!!
+		uint _id = totalOwnedItems++;
+		ownedItemsHash[_id] = itemHash;
+		ownedItems[itemHash] = Item(
+			_id, 
+			msg.value, 
+			_proof, 
+			msg.sender, 
+			State.Purchased
+		);
+	
 	}
 }
