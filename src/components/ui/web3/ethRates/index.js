2165cff01de116c5c7e00057f71b338ef4ba04f3
Tenzija <milosmiskov.miki@gmail.com>
2023-03-06 16:57:04 +0100

    loader done ☑

--- src/components/ui/web3/ethRates/index.js
+++ src/components/ui/web3/ethRates/index.js
@@ -1,4 +1,4 @@
-import { useEthPrice } from "@/components/hooks/useEthPrice"
+import { useEthPrice, coursePrice } from "@/components/hooks/useEthPrice"
 import Image from "next/image"
 import { Loader } from "../../common"
 
@@ -10,10 +10,7 @@ export default function EthRates() {
       <div className="flex flex-1 items-stretch text-center">
         <div className="p-10 border drop-shadow rounded-md">
           <div className='flex items-center'>
-            { true ? 
-              <div className="grid grid-cols-5"><div className="col-start-3"><Loader /></div></div>
-              
-               :
+            { data['eth'].data ? 
               <> 
                 <Image
                   layout="fixed"
@@ -23,6 +20,8 @@ export default function EthRates() {
                 />
                 <span className="text-2xl font-bold"> = {data['eth'].data}$</span>
               </>
+              :
+              <div className="grid grid-cols-5"><div className="col-start-3"><Loader /></div></div>
             }
           </div>
           <p className="text-xl text-gray-500">Current eth Price</p>
@@ -31,19 +30,23 @@ export default function EthRates() {
       <div className="flex flex-1 items-stretch text-center">
         <div className="p-10 border drop-shadow rounded-md">
           <div className='flex items-center'>
-            <span className="text-2xl font-bold">
-              {data['eth'].perItem}
-            </span>
-            <Image
-              layout="fixed"
-              width={35}
-              height={35}
-              src='/small-eth.webp'
-            />
-            <span className='text-2xl font-bold'>
-              = 15$
-            </span>
-            
+            { data['eth'].data ?
+              <>
+                <span className="text-2xl font-bold">
+                  {data['eth'].perItem}
+                </span>
+                <Image
+                  layout="fixed"
+                  width={35}
+                  height={35}
+                  src='/small-eth.webp'
+                />
+                <span className='text-2xl font-bold'>
+                  = {coursePrice}$
+                </span>
+              </> :
+              <div className="grid grid-cols-5"><div className="col-start-3"><Loader /></div></div>
+            }
           </div>
           <p className="text-xl text-gray-500">Price per Item</p>
         </div>
