674b76c52a7e1058fbd056f37241ea7d64156f9c
Tenzija <milosmiskov.miki@gmail.com>
2023-03-10 18:53:46 +0100

    Update owned.js

--- src/pages/marketplace/courses/owned.js
+++ src/pages/marketplace/courses/owned.js
@@ -24,6 +24,7 @@ export default function OwnedCourses({courses}) {
 
     return (
         <>
+        
             <MarketHeader />
             <section className='grid grid-cols-1'>
                 { account.isEmpty ? <Message type="INFO">Please connect to Metamask</Message> :
