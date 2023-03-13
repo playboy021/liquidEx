b67a7f56e15c04d0514471d838eb13645e559679
Tenzija <milosmiskov.miki@gmail.com>
2023-03-13 18:29:27 +0100

    Home Hero work

--- src/components/ui/common/hero/index.js
+++ src/components/ui/common/hero/index.js
@@ -2,9 +2,50 @@ export default function Hero() {
 
   return (
     <section className="lg:2/6 text-left my-28">
-      <div className="text-6xl font-semibold text-gray-900 leading-none">Grow your career as a developer</div>
-      <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">Learn programming and web development the easy way! Get unlimited access to all of our courses.</div>
-      <div className="mt-5 sm:mt-8 flex lg:justify-start">
+      <div className="text-6xl font-semibold text-gray-900 leading-none">
+        <svg width="100%" height="0%">
+          <text x="0%" y="80%">
+            LiquidEX
+          </text>
+        </svg>
+      </div>
+      <div className="mt-6 text-xl font-light text-true-gray-500 antialiased">
+
+      <div className="hero">
+        <span className="text-lg">Lorem</span>&nbsp;
+        <span className="text-lg">ipsum</span>&nbsp;
+        <span className="text-lg">dolor</span>&nbsp;
+        <span className="text-lg">amet,</span>&nbsp;
+        <span className="text-lg">consectetur</span>&nbsp;
+        <span className="text-lg">adipiscing</span>&nbsp;
+        <span className="text-lg">elit.</span>&nbsp;
+        <span className="text-lg">Nullam</span>&nbsp;
+        <span className="text-lg">cursus</span>&nbsp;
+        <span className="text-lg">tincidunt</span>&nbsp;
+        <span className="text-lg">tempus.</span>&nbsp;
+        <span className="text-lg">Morbi</span>&nbsp;
+        <span className="text-lg">porttitor</span>&nbsp;
+        <span className="text-lg">neque</span>&nbsp;
+        <span className="text-lg">eget</span>&nbsp;
+        <span className="text-lg">ipsum</span>&nbsp;
+        <span className="text-lg">tempus</span>&nbsp;
+        <span className="text-lg">egestas.</span>&nbsp;
+        <span className="text-lg">Ut</span>&nbsp;
+        <span className="text-lg">luctus</span>&nbsp;
+        <span className="text-lg">gravida.</span>&nbsp;
+        <span className="text-lg">Maecenas</span>&nbsp;
+        <span className="text-lg">rutrum</span>&nbsp;
+        <span className="text-lg">ligula</span>&nbsp;
+        <span className="text-lg">non</span>&nbsp;
+        <span className="text-lg">mi</span>&nbsp;
+        <span className="text-lg">facilisis,</span>&nbsp;
+        <span className="text-lg">id</span>&nbsp;
+        <span className="text-lg">mattis</span>&nbsp;
+        <span className="text-lg">quam.</span>&nbsp;
+      </div>
+
+      </div>
+      <div className="mt-4 flex lg:justify-start">
         <div className="rounded-md shadow">
           <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
             Get started
