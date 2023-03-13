e31a92477e910f7e1a473d09ca76823ded2a11cf
Tenzija <milosmiskov.miki@gmail.com>
2023-03-13 16:02:30 +0100

    Update useManagedItems.js

--- src/components/providers/web3/hooks/useManagedItems.js
+++ src/components/providers/web3/hooks/useManagedItems.js
@@ -1,4 +1,3 @@
-import { createItemHash } from "@/utils/hash"
 import { normalizeOwnedItem } from "@/utils/normalize"
 import useSWR from "swr"
 
@@ -6,7 +5,19 @@ export const handler = (web3, contract) => account => {
     const swrRes = useSWR(() => 
         (web3 && contract && account) ? `web3/managedItems/${account}` : null,
         async () => {
-            const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+            const items = []
+
+            const itemCount = await contract.getItemCount()
+
+            for (let i = Number(itemCount) - 1; i >= 0 ; i--) {
+                const itemHash = await contract.getItemHashAtIndex(i)
+                const item = await contract.getItemByHash(itemHash)
+                
+                if (item) {
+                    const normalize = normalizeOwnedItem(web3)({ hash: itemHash }, item)
+                    items.push(normalize)
+                }
+            }
 
             return items
         }
