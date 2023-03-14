141f5eb7cc027da11540d9c3a5fe3c83af9b97e3
Tenzija <milosmiskov.miki@gmail.com>
2023-03-14 15:57:59 +0100

    item Verification ✅

--- src/pages/marketplace/courses/managed.js
+++ src/pages/marketplace/courses/managed.js
@@ -1,22 +1,48 @@
 import { useAccount, useManagedItems } from "@/components/hooks/web3";
-import { Button } from "@/components/ui/common";
+import { Button, Message } from "@/components/ui/common";
 import { LoaderBig } from "@/components/ui/common/loader";
-import { CourseFilter, ManagedItemCard, OwnedCourseCard } from "@/components/ui/course";
+import { CourseFilter, ManagedItemCard } from "@/components/ui/course";
 import { BaseLayout } from "@/components/ui/layout";
 import { MarketHeader } from "@/components/ui/marketplace";
+import { ethers } from "ethers";
 import { useState } from "react";
 
 export default function ManagedCourses() {
     const [email, setEmail] = useState('')
+    const [proofOfOwnership, setProofOfOwnership] = useState({})
 
     const { account } = useAccount()
     const { managedItems } = useManagedItems(account.data)
 
     const verifyItem = (email, {hash, proof}) => {
-        console.log('email', email)
-        console.log('hash', hash)
-        console.log('proof', proof)
+        const emailHash = ethers.utils.solidityKeccak256(
+            ['string'], [email]
+        )
+
+        const proofToCheck = ethers.utils.solidityKeccak256(
+            ['bytes32', 'bytes32'],
+            [emailHash, hash]
+        )
+
+        // One method to verify the proof
+        // if (proofToCheck === proof) {
+        //     alert('Item is verified!')
+        // } else {
+        //     alert('Item is not verified!')
+        // }
+
+        // Another method to verify the proof
+        proofToCheck === proof ?
+            setProofOfOwnership({
+                [hash]: true
+            }) :
+            setProofOfOwnership({
+                [hash]: false
+            })
+
     }
+
+    
 
     return (
         <>
@@ -33,12 +59,12 @@ export default function ManagedCourses() {
                             <div className="flex mr-2 relative rounded-md">
                                 <input 
                                     value={email}
-                                    onChange={e => setEmail(e.target.value)}
-                                    type="text"
+                                    onChange={({target: {value}}) => setEmail(value)}
+                                    type="email"
                                     name="account"
                                     id="account"
-                                    className="w-96 foucs:ring-indigo-500 shadow-md focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-md pl-7 p-4"
-                                    placeholder='0x2341ab...'
+                                    className="w-96 foucs:ring-indigo-500 shadow-md focus:border-indigo-500 block sm:text-sm border-gray-300 rounded-md pl-7 p-4 text-black"
+                                    placeholder='placeholder@mail.com'
                                 />
                                 <Button 
                                     className='ml-3' 
@@ -49,6 +75,20 @@ export default function ManagedCourses() {
                                     Verify
                                 </Button>
                             </div>
+                            { proofOfOwnership[item.hash] &&
+                                <div className="mt-4">
+                                    <Message>
+                                        Item is verified!
+                                    </Message>
+                                </div>
+                            }
+                            { proofOfOwnership[item.hash] == false &&
+                                <div className="mt-4">
+                                    <Message type="ERROR" >
+                                        Item is not verified!
+                                    </Message>
+                                </div>
+                            }
                         </ManagedItemCard>
                 )}
             </section>
