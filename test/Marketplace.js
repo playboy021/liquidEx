c042e080c40f8f264913756e4abd0cf2201b83aa
Tenzija <milosmiskov.miki@gmail.com>
2023-03-15 17:29:03 +0100

    test init

---
+++ test/Marketplace.js
@@ -0,0 +1,114 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { expect } = require("chai");
+
+describe("Marketplace", function () {
+
+  async function deployMarketplaceFixture() {
+
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const Marketplace = await ethers.getContractFactory("Marketplace");
+    const marketplace = await Marketplace.deploy();
+
+    // for value use this { value: lockedAmount }
+
+    return { marketplace, owner, otherAccount };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      const { owner, marketplace, otherAccount } = await loadFixture(deployMarketplaceFixture);
+
+      expect(await marketplace.getContractOwner()).to.equal(owner.address);
+    });
+
+  //   it("Should receive and store the funds to lock", async function () {
+  //     const { lock, lockedAmount } = await loadFixture(
+  //       deployMarketplaceFixture
+  //     );
+
+  //     expect(await ethers.provider.getBalance(lock.address)).to.equal(
+  //       lockedAmount
+  //     );
+  //   });
+
+  //   it("Should fail if the unlockTime is not in the future", async function () {
+  //     // We don't use the fixture here because we want a different deployment
+  //     const latestTime = await time.latest();
+  //     const Lock = await ethers.getContractFactory("Lock");
+  //     await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
+  //       "Unlock time should be in the future"
+  //     );
+  //   });
+  // });
+
+  // describe("Withdrawals", function () {
+  //   describe("Validations", function () {
+  //     it("Should revert with the right error if called too soon", async function () {
+  //       const { lock } = await loadFixture(deployMarketplaceFixture);
+
+  //       await expect(lock.withdraw()).to.be.revertedWith(
+  //         "You can't withdraw yet"
+  //       );
+  //     });
+
+  //     it("Should revert with the right error if called from another account", async function () {
+  //       const { lock, unlockTime, otherAccount } = await loadFixture(
+  //         deployMarketplaceFixture
+  //       );
+
+  //       // We can increase the time in Hardhat Network
+  //       await time.increaseTo(unlockTime);
+
+  //       // We use lock.connect() to send a transaction from another account
+  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+  //         "You aren't the owner"
+  //       );
+  //     });
+
+  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+  //       const { lock, unlockTime } = await loadFixture(
+  //         deployMarketplaceFixture
+  //       );
+
+  //       // Transactions are sent using the first signer by default
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).not.to.be.reverted;
+  //     });
+  //   });
+
+  //   describe("Events", function () {
+  //     it("Should emit an event on withdrawals", async function () {
+  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
+  //         deployMarketplaceFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw())
+  //         .to.emit(lock, "Withdrawal")
+  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+  //     });
+  //   });
+
+  //   describe("Transfers", function () {
+  //     it("Should transfer the funds to the owner", async function () {
+  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+  //         deployMarketplaceFixture
+  //       );
+
+  //       await time.increaseTo(unlockTime);
+
+  //       await expect(lock.withdraw()).to.changeEtherBalances(
+  //         [owner, lock],
+  //         [lockedAmount, -lockedAmount]
+  //       );
+  //     });
+  //  });
+  });
+});
