4c802ea1f36a6c7e69938eb0ea062c8736fe187b
Tenzija <milosmiskov.miki@gmail.com>
2023-04-02 22:13:13 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
       'xs' : '450px',
